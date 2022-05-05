dea190c2d2614675f5613666c4b7ac8af33bdf05
Alexander Peev <103655534+sashimiP@users.noreply.github.com>
2022-05-05 17:41:52 +0300

    Update Window.h

--- Source/Headers/Window.h
+++ Source/Headers/Window.h
@@ -1,62 +1,36 @@
-#include "Window.h"
+#pragma once
+#include<SFML/Graphics.hpp>
+#include<string>
 
-Window::Window() { Setup("Axe", sf::Vector2u(800, 450)); }
-Window::Window(const std::string& title, const sf::Vector2u& size)
+class Window
 {
-	Setup(title, size);
-}
-Window::~Window() { Destroy(); }
+public:
+	Window();
+	Window(const std::string& title, const sf::Vector2u& size);
+	~Window();
 
-void Window::Setup(const std::string title, const sf::Vector2u& size)
-{
-	m_windowTitle = title;
-	m_windowSize = size;
-	m_isFullScreen = false;
-	m_isDone = false;
-	Create();
-}
+	void BeginDraw();
+	void EndDraw();
 
-void Window::Create()
-{
-	auto style = (m_isFullScreen ? sf::Style::Fullscreen
-		: sf::Style::Default);
-	m_window.create({ m_windowSize.x, m_windowSize.y, 32 },
-		m_windowTitle, style);
-}
+	void Update();
 
-void Window::Destroy()
-{
-	m_window.close();
-}
+	bool IsDone();
+	bool isFullScreen();
+	sf::RenderWindow* GetRenderWindow();
+	sf::Vector2u GetWindowSize();
 
-void Window::BeginDraw() { m_window.clear(sf::Color::Black); }
-void Window::EndDraw() { m_window.display(); }
+	void ToggleFullScreen();
 
-bool Window::IsDone() { return m_isDone; }
-bool Window::isFullScreen() { return m_isFullScreen; }
+private:
+	void Setup(const std::string title, const sf::Vector2u& size);
+	void Create();
+	void Destroy();
 
-sf::RenderWindow* Window::GetRenderWindow() { return &m_window; }
-sf::Vector2u Window::GetWindowSize() { return m_windowSize; }
+	sf::RenderWindow m_window;
+	sf::Vector2u m_windowSize;
+	std::string m_windowTitle;
+	bool m_isDone;
+	bool m_isFullScreen;
+	sf::Event m_event;
+};
 
-void Window::ToggleFullScreen() {
-	m_isFullScreen = !m_isFullScreen;
-	m_window.close();
-	Create();
-}
-
-void Window::Update() {
-	while (m_window.pollEvent(m_event))
-	{
-		if (m_event.type == sf::Event::Closed) {
-			m_isDone = true;
-		}
-		else if (m_event.type == sf::Event::KeyPressed && m_event.key.code == sf::Keyboard::Escape)
-		{
-			m_isDone = true;
-		}
-		else if (m_event.type == sf::Event::KeyPressed && m_event.key.code == sf::Keyboard::F5)
-		{
-			ToggleFullScreen();
-		}
-	}
-}
